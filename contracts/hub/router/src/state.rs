ed1eb39517fee40cd7728a80619d4274ea47c4d2
Anshudhar Kumar Singh <anshudhar2001@gmail.com>
2024-06-04 13:39:15 +0530

    rename state namespace in router for consistency

--- contracts/hub/router/src/state.rs
+++ contracts/hub/router/src/state.rs
@@ -15,5 +15,5 @@ pub const STATE: Item<State> = Item::new("state");
 // Convert it to multi index map?
 pub const VLPS: Map<(Token, Token), String> = Map::new("vlps");
 
-pub const CHAIN_ID_TO_CHAIN: Map<String, Chain> = Map::new("chains");
-pub const CHANNEL_TO_CHAIN_ID: Map<String, String> = Map::new("channels_to_chain_id");
+pub const CHAIN_ID_TO_CHAIN: Map<String, Chain> = Map::new("chain_id_to_chain");
+pub const CHANNEL_TO_CHAIN_ID: Map<String, String> = Map::new("channel_to_chain_id");
