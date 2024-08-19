eac54f32d96ebe91172d66151b7a97d87e89a595
Joe Monem <jmonem@icloud.com>
2024-08-19 11:28:24 +0200

    feat: validate method fro TokenWithDenom

--- packages/euclid/src/token.rs
+++ packages/euclid/src/token.rs
@@ -328,6 +328,21 @@ pub struct TokenWithDenom {
 }
 
 impl TokenWithDenom {
+    /// Validates smart contract addresses, checks against empty denom and zero supply
+    pub fn validate(&self, deps: Deps) -> Result<(), ContractError> {
+        if self.token_type.is_smart() {
+            let denom = self.token_type.get_denom();
+            let potential_supply = deps.querier.query_supply(denom.clone())?;
+            let non_empty_denom = !denom.is_empty();
+            let non_zero_supply = !potential_supply.amount.is_zero();
+            ensure!(
+                non_empty_denom && non_zero_supply,
+                ContractError::InvalidAsset { asset: denom }
+            );
+        }
+        Ok(())
+    }
+
     pub fn get_denom(&self) -> String {
         self.token_type.get_denom()
     }
@@ -364,28 +379,9 @@ impl PairWithDenom {
             self.token_1.token != self.token_2.token,
             ContractError::DuplicateTokens {}
         );
-        // Validate smart contract addresses
-        if self.token_1.token_type.is_smart() {
-            let denom = self.token_1.token_type.get_denom();
-            let potential_supply = deps.querier.query_supply(denom.clone())?;
-            let non_empty_denom = !denom.is_empty();
-            let non_zero_supply = !potential_supply.amount.is_zero();
-            ensure!(
-                non_empty_denom && non_zero_supply,
-                ContractError::InvalidAsset { asset: denom }
-            );
-        }
-
-        if self.token_2.token_type.is_smart() {
-            let denom = self.token_2.token_type.get_denom();
-            let potential_supply = deps.querier.query_supply(denom.clone())?;
-            let non_empty_denom = !denom.is_empty();
-            let non_zero_supply = !potential_supply.amount.is_zero();
-            ensure!(
-                non_empty_denom && non_zero_supply,
-                ContractError::InvalidAsset { asset: denom }
-            );
-        }
+        // Validate each token
+        self.token_1.validate(deps)?;
+        self.token_2.validate(deps)?;
 
         Ok(())
     }
