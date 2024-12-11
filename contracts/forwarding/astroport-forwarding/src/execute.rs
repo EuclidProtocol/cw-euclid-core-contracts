3267ea977f8a92314f63c254988296be41b2f46b
Anshudhar Kumar Singh <anshudhar2001@gmail.com>
2024-12-11 14:02:02 +0530

    fix: remove commented code

--- contracts/forwarding/astroport-forwarding/src/execute.rs
+++ contracts/forwarding/astroport-forwarding/src/execute.rs
@@ -19,8 +19,6 @@ pub fn execute_cw20_receive(
     info: &MessageInfo,
     msg: Cw20ReceiveMsg,
 ) -> Result<Response, ContractError> {
-    // let state = STATE.load(deps.storage)?;
-    // let sender = msg.sender;
 
     let amount = msg.amount;
     let from_token = TokenType::Smart {
