2ca4e49f1ef9758a13414f8805ee5d83db60f443
Anshudhar Kumar Singh <anshudhar2001@gmail.com>
2025-04-02 14:59:41 +0530

    fix: use swap amount in stable swap

--- contracts/hub/stable_vlp/src/execute.rs
+++ contracts/hub/stable_vlp/src/execute.rs
@@ -415,6 +415,7 @@ pub fn execute_swap(
         .lp_fees
         .add_fee(asset_in.to_string(), lp_fee);
 
+
     // Calcuate the sum of fees
     let total_fee = lp_fee.checked_add(euclid_fee)?;
 
@@ -424,7 +425,7 @@ pub fn execute_swap(
     let amp_factor = AMP_FACTOR.load(deps.storage).unwrap_or(DEFAULT_AMP_FACTOR);
 
     let receive_amount = compute_swap(
-        &Decimal256::from_integer(amount_in),
+        &Decimal256::from_integer(swap_amount),
         &Decimal256::from_integer(token_in_reserve),
         &Decimal256::from_integer(token_out_reserve),
         amp_factor,
