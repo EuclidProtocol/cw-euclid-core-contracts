c6eb1f854961a98f069693e79857ff1a7078b9d1
Joe Monem <jmonem@icloud.com>
2026-01-07 14:49:23 +0200

    fix: use sender chain uid

--- contracts/hub/virtual_balance/src/execute.rs
+++ contracts/hub/virtual_balance/src/execute.rs
@@ -128,8 +128,8 @@ pub fn execute_transfer(
     // Check if voucher is paused
     token_pause_check(
         deps.storage,
-        //TODO This originally took the sender's chain uid but what do we do in case the sender in transfer_msg is empty?
-        transfer_msg.to.chain_uid.clone(),
+        //TODO What do we do in case the sender in transfer_msg is empty?
+        sender.chain_uid.clone(),
         transfer_msg.token_id.clone(),
     )?;
 
